44d6f6ee101c80f751a7603434fb8671b119e91e
MukhudwaneLucky <mukhudwane17@gmail.com>
2021-06-07 18:02:50 +0200

    added contact form, but not complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -227,3 +227,30 @@
   }
 
 })()
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+// form email/contact
+function sendEmail(){
+	window.location.href = "mailto:mukhudwane17@gmail.com";
+	document.getElementById("error-message").innerHTML = "Under Construction....Coming Soon." ;
+}
