827a0ede11c6765b0a4b37d76d2b034e6043de6e
MukhudwaneLucky <mukhudwane17@gmail.com>
2022-04-19 14:59:35 +0200

    updated portfolio

---
+++ main.js
@@ -0,0 +1,11 @@
+function view_blog(){
+    window.open('https://mukhudwanelucky.github.io/blog/', '_blank');
+}
+
+function view_blog_repo(){
+    window.open('https://github.com/MukhudwaneLucky/blog', '_blank');
+}
+
+function view_react_task_tracker_repo(){
+    window.open('https://github.com/MukhudwaneLucky/react-task-tracker', '_blank');
+}
